f3484adcc60480491db9c148650248382ef19925
tianxiangbing <55342775@qq.com>
2016-11-17 18:23:20 +0800

    ball

--- dist/DodgeBall.js
+++ dist/DodgeBall.js
@@ -4,55 +4,57 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /// <reference path="index.ts" />
-var G = (function (_super) {
-    __extends(G, _super);
-    function G() {
-        _super.apply(this, arguments);
-        this.count = 1;
-    }
-    G.prototype.newGame = function () {
-        this.count = 1;
-        _super.prototype.newGame.call(this);
-        this.createRole();
-    };
-    G.prototype.running = function () {
-        this.count++;
-        if (this.count > 10000) {
-            this.over();
-            return;
+(function () {
+    var G = (function (_super) {
+        __extends(G, _super);
+        function G() {
+            _super.apply(this, arguments);
+            this.count = 1;
         }
-        // console.log(this.count)
-        this.context.clearRect(0, 0, this.stage.width, this.stage.height);
-        this.role.draw();
-        _super.prototype.running.call(this);
-    };
-    G.prototype.gameOver = function () {
-        _super.prototype.gameOver.call(this);
-    };
-    G.prototype.loading = function () {
-        console.log('loading...');
-        _super.prototype.loading.call(this);
-    };
-    //创建英雄
-    G.prototype.createRole = function () {
-        this.role = new Sprite(this.context, 'ball.png');
-        this.role.setSize(this.stage.width / 3, this.stage.width / 3);
-        this.role.setPosition(this.stage.width / 3, this.stage.height - this.role.h);
-    };
-    return G;
-}(JY));
-var view = document.getElementById('view');
-var w = view.offsetWidth;
-var h = view.offsetHeight;
-var stage = new Stage(w, h);
-var descript = new Discript('start');
-descript.text = '<p class="title">DodgeBall</p>';
-var gameOver = new GameOver('restart');
-var title = new Title('DodgeBall');
-var control = new Control();
-control.rect = [100, 100];
-var game = new G(view, stage, title, descript, gameOver);
-game.files = { image: ['ball.png', 'boll.jpg'] };
-game.setup();
+        G.prototype.newGame = function () {
+            this.count = 1;
+            this.createRole();
+            _super.prototype.newGame.call(this);
+        };
+        G.prototype.running = function () {
+            this.count++;
+            if (this.count > 10000) {
+                this.over();
+                return;
+            }
+            // console.log(this.count)
+            this.context.clearRect(0, 0, this.stage.width, this.stage.height);
+            this.role.draw();
+            _super.prototype.running.call(this);
+        };
+        G.prototype.gameOver = function () {
+            _super.prototype.gameOver.call(this);
+        };
+        G.prototype.loading = function () {
+            console.log('loading...');
+            _super.prototype.loading.call(this);
+        };
+        //创建英雄
+        G.prototype.createRole = function () {
+            this.role = new Sprite(this.context, 'ball.png');
+            this.role.setSize(this.stage.width / 3, this.stage.width / 3);
+            this.role.setPosition(this.stage.width / 3, this.stage.height - this.role.h);
+        };
+        return G;
+    }(JY));
+    var view = document.getElementById('view');
+    var w = view.offsetWidth;
+    var h = view.offsetHeight;
+    var stage = new Stage(w, h);
+    var descript = new Discript('start');
+    descript.text = '<p class="title">DodgeBall</p>';
+    var gameOver = new GameOver('restart');
+    var title = new Title('DodgeBall');
+    var control = new Control();
+    control.rect = [100, 100];
+    var game = new G(view, stage, title, descript, gameOver);
+    game.files = { image: ['ball.png', 'boll.jpg'] };
+    game.setup();
+})();
 
 //# sourceMappingURL=DodgeBall.js.map
--- src/DodgeBall.ts
+++ src/DodgeBall.ts
@@ -1,47 +1,49 @@
 /// <reference path="index.ts" />
-class G extends JY {
-    count = 1;
-    role:Sprite;
-    newGame() {
-        this.count = 1;
-        super.newGame();
-        this.createRole();
-    }
-    running() {
-        this.count++;
-        if (this.count > 10000) {
-            this.over();
-            return;
+(function(){
+    class G extends JY {
+        count = 1;
+        role:Sprite;
+        newGame() {
+            this.count = 1;
+            this.createRole();
+            super.newGame();
         }
-        // console.log(this.count)
-        this.context.clearRect(0,0,this.stage.width,this.stage.height);
-        this.role.draw();
-        super.running();
+        running() {
+            this.count++;
+            if (this.count > 10000) {
+                this.over();
+                return;
+            }
+            // console.log(this.count)
+            this.context.clearRect(0,0,this.stage.width,this.stage.height);
+            this.role.draw();
+            super.running();
+        }
+        gameOver() {
+            super.gameOver();
+        }
+        loading() {
+            console.log('loading...')
+            super.loading();
+        }
+        //创建英雄
+        createRole(){
+            this.role = new Sprite(this.context,'ball.png');
+            this.role.setSize(this.stage.width/3,this.stage.width/3);
+            this.role.setPosition(this.stage.width/3,this.stage.height - this.role.h);
+        }
     }
-    gameOver() {
-        super.gameOver();
-    }
-    loading() {
-        console.log('loading...')
-        super.loading();
-    }
-    //创建英雄
-    createRole(){
-        this.role = new Sprite(this.context,'ball.png');
-        this.role.setSize(this.stage.width/3,this.stage.width/3);
-        this.role.setPosition(this.stage.width/3,this.stage.height - this.role.h);
-    }
-}
-let view = document.getElementById('view');
-let w = view.offsetWidth;
-let h = view.offsetHeight;
-let stage = new Stage(w, h);
-let descript = new Discript('start');
-descript.text= '<p class="title">DodgeBall</p>'
-let gameOver = new GameOver('restart');
-let title = new Title('DodgeBall');
-let control = new Control();
-control.rect = [100, 100]
-let game = new G(view, stage, title, descript, gameOver);
-game.files = {image: ['ball.png','boll.jpg']};
-game.setup();
+    let view = document.getElementById('view');
+    let w = view.offsetWidth;
+    let h = view.offsetHeight;
+    let stage = new Stage(w, h);
+    let descript = new Discript('start');
+    descript.text= '<p class="title">DodgeBall</p>'
+    let gameOver = new GameOver('restart');
+    let title = new Title('DodgeBall');
+    let control = new Control();
+    control.rect = [100, 100]
+    let game = new G(view, stage, title, descript, gameOver);
+    game.files = {image: ['ball.png','boll.jpg']};
+    game.setup();
+})();
